dc7f3e8ae8e561ee134c0b39abc75032fd5857b0
Ludvig <kalbergludvig@outlook.com>
2026-04-06 18:43:38 +0200

    add install printer driver and add user to lpadmin

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade
 sudo apt install cups
+
+sudo apt install printer-driver-brlaser # installs drivers for brother printers
+
+sudo usermod -aG lpadmin $USER
