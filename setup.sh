691f4a159567d1df67dec6d5cd025c10e3e6ca0a
Ludvig <kalbergludvig@outlook.com>
2026-04-06 20:21:04 +0200

    add web interface access from local network

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ sudo apt install printer-driver-brlaser # installs drivers for brother printers
 sudo usermod -aG lpadmin $USER
 
 sudo sed -i "s/Listen localhost:631/Listen $(hostname).local:631/" /etc/cups/cupsd.conf
+
+sudo sed -i "/<Location \/>/a\ Allow @local" /etc/cups/cupsd.conf
+sudo sed -i "/<Location \/admin>/a\ Allow @local" /etc/cups/cupsd.conf
+sudo sed -i "/<Location \/admin\/conf>/a\ Allow @local" /etc/cups/cupsd.conf
+sudo systemctl restart cups
