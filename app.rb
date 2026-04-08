6920fcfaa48750cf3ebb22cf8780465b7e988eda
Ludvig Kalberg <kalbergludvig@outlook.com>
2026-04-08 13:12:06 +0200

    add file upload form

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+include FileUtils::Verbose
 
 get '/' do
-  'Hello World'
+  "<h1>Print Server</h1>"
 end
+
+get '/upload' do 
+  erb :upload 
+end
+
+__END__
+
+@@upload
+<form action='/upload' enctype="multipart/form-data" method='POST'>     
+  <input name="file" type="file" />     
+  <input type="submit" value="Upload" /> 
+</form>
